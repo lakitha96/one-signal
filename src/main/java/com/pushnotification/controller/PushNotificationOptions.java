ce91b11a5d6bfeeb39c4d51b42de186accfd643e
lakitha96 <lakithaprabudh96@gmail.com>
2018-07-31 17:11:40 +0530

    1st commit

---
+++ src/main/java/com/pushnotification/controller/PushNotificationOptions.java
@@ -0,0 +1,112 @@
+package com.pushnotification.controller;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class PushNotificationOptions {
+
+    public static final String REST_API_KEY = "ZjIzNzViMTUtZDk2Zi00ODJiLTg3YjItYzU3ZjMyMTkyNTVl";
+    public static final String APP_ID = "91a8935c-be69-41a8-b122-9b8625b41916";
+
+    public static void sendMessageToAllUsers(String message) {
+        try {
+            String jsonResponse;
+
+            URL url = new URL("https://onesignal.com/api/v1/notifications");
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setUseCaches(false);
+            con.setDoOutput(true);
+            con.setDoInput(true);
+
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            con.setRequestProperty("Authorization", "Basic ZjIzNzViMTUtZDk2Zi00ODJiLTg3YjItYzU3ZjMyMTkyNTVl");//REST API
+            con.setRequestMethod("POST");
+
+            String strJsonBody = "{"
+                    +   "\"app_id\": \""+ APP_ID +"\","
+                    +   "\"included_segments\": [\"All\"],"
+                    +   "\"data\": {\"foo\": \"bar\"},"
+                    +   "\"contents\": {\"en\": \""+ message +"\"}"
+                    + "}";
+
+
+            System.out.println("strJsonBody:\n" + strJsonBody);
+
+            byte[] sendBytes = strJsonBody.getBytes("UTF-8");
+            con.setFixedLengthStreamingMode(sendBytes.length);
+
+            OutputStream outputStream = con.getOutputStream();
+            outputStream.write(sendBytes);
+
+            int httpResponse = con.getResponseCode();
+            System.out.println("httpResponse: " + httpResponse);
+
+            jsonResponse = mountResponseRequest(con, httpResponse);
+            System.out.println("jsonResponse:\n" + jsonResponse);
+
+        } catch(Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private static String mountResponseRequest(HttpURLConnection con, int httpResponse) throws IOException {
+        String jsonResponse;
+        if (  httpResponse >= HttpURLConnection.HTTP_OK
+                && httpResponse < HttpURLConnection.HTTP_BAD_REQUEST) {
+            Scanner scanner = new Scanner(con.getInputStream(), "UTF-8");
+            jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+            scanner.close();
+        }
+        else {
+            Scanner scanner = new Scanner(con.getErrorStream(), "UTF-8");
+            jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+            scanner.close();
+        }
+        return jsonResponse;
+    }
+
+    public static void sendMessageToUser(
+            String message, String userId) {
+        try {
+            String jsonResponse;
+
+            URL url = new URL("https://onesignal.com/api/v1/notifications");
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setUseCaches(false);
+            con.setDoOutput(true);
+            con.setDoInput(true);
+
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            con.setRequestProperty("Authorization", "Basic ZjIzNzViMTUtZDk2Zi00ODJiLTg3YjItYzU3ZjMyMTkyNTVl");
+            con.setRequestMethod("POST");
+
+            String strJsonBody = "{"
+                    +   "\"app_id\": \""+ APP_ID +"\","
+                    +   "\"include_player_ids\": [\""+ userId +"\"],"
+                    +   "\"data\": {\"foo\": \"bar\"},"
+                    +   "\"contents\": {\"en\": \""+ message +"\"}"
+                    + "}";
+
+
+            System.out.println("strJsonBody:\n" + strJsonBody);
+
+            byte[] sendBytes = strJsonBody.getBytes("UTF-8");
+            con.setFixedLengthStreamingMode(sendBytes.length);
+
+            OutputStream outputStream = con.getOutputStream();
+            outputStream.write(sendBytes);
+
+            int httpResponse = con.getResponseCode();
+            System.out.println("httpResponse: " + httpResponse);
+
+            jsonResponse = mountResponseRequest(con, httpResponse);
+            System.out.println("jsonResponse:\n" + jsonResponse);
+
+        } catch(Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
